cd084a25667ba94b17d8da02229383c5133e14a2
Nisham Karki <37975427+NishamKarki@users.noreply.github.com>
2026-09-09 03:31:38 -0500

    Integrating wails instead of fyne

--- main.go
+++ main.go
@@ -1,14 +1,36 @@
 package main
 
 import (
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/app"
+	"embed"
+
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 )
 
-func main() {
-	a := app.New()
-	w := a.NewWindow("FileSync")
-	w.Resize(fyne.NewSize(700, 550))
+//go:embed all:frontend/dist
+var assets embed.FS
 
-	w.ShowAndRun()
+func main() {
+	// Create an instance of the app structure
+	app := NewApp()
+
+	// Create application with options
+	err := wails.Run(&options.App{
+		Title:  "FileSync",
+		Width:  1024,
+		Height: 768,
+		AssetServer: &assetserver.Options{
+			Assets: assets,
+		},
+		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		OnStartup:        app.startup,
+		Bind: []interface{}{
+			app,
+		},
+	})
+
+	if err != nil {
+		println("Error:", err.Error())
+	}
 }
