afa31d85dafbf92135a51c907c9f21b2c364766a
Nisham Karki <37975427+NishamKarki@users.noreply.github.com>
2026-09-11 00:05:22 -0500

    testing push before pulling merged contents

--- main.go
+++ main.go
@@ -18,8 +18,8 @@ func main() {
 	// Create application with options
 	err := wails.Run(&options.App{
 		Title:  "FileSync",
-		Width:  1024,
-		Height: 768,
+		Width:  920,
+		Height: 700,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
